ec468d41c8895af4b1cc1ed27223686b2bd93884
Terryroud <kolibri.alia@yandex.ru>
2025-07-12 17:06:00 +0300

    fix settings

--- stylechanging/settings.py
+++ stylechanging/settings.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-**************************************************'
-
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
