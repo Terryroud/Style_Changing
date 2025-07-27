7a788f4abe979292891e769d42ade87715e5071b
Terryroud <kolibri.alia@yandex.ru>
2025-07-27 13:41:01 +0300

    change settings for host

--- stylechanging/settings.py
+++ stylechanging/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # Разрешает все хосты (на время теста)
 
 INSTALLED_APPS = [
     'django.contrib.admin',
